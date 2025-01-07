1f6e453600a6a60d047eb4fd6b1078a4c67f9737
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2025-01-07 19:33:37 +0100

    removed layer normalization

--- src/models/lstm_model.py
+++ src/models/lstm_model.py
@@ -6,7 +6,6 @@ class LSTM(nn.Module):
         super(LSTM, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
-        self.layer_norm = nn.LayerNorm(hidden_size) 
         self.dropout = nn.Dropout(p=dropout)
         self.fc = nn.Linear(hidden_size, 1)
 
@@ -14,7 +13,6 @@ class LSTM(nn.Module):
         x = self.embedding(x)  
         x = torch.mul(x, attention_mask.unsqueeze(2)) 
         lstm_out, _ = self.lstm(x)  
-        lstm_out = self.layer_norm(lstm_out)  
         lstm_out = self.dropout(lstm_out)
         out = self.fc(lstm_out[:, -1, :])  
         return out
